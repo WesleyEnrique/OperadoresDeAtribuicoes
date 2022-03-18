b94bce515d57d7f51f7631c5b4698efc6d0e7fbd
WesleyEnrique <wesleyarauoj18@gmail.com>
2022-03-17 23:25:24 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+a = float(input("Digite um valor real: "))
+b = int (input("Digite um valor ineiro: "))
+produto = a*b
+print("produto é", produto)
+
+#Vamos demonstrar a utilização do operador de atribuição +=
+soma = 0 
+a = float(input("Digite um valor real: "))
+soma += a 
+a = int(input("Digite um valor inteiro: "))
+soma += a
+print("A soma é", soma)
